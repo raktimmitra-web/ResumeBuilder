b6cbef932be4556ee1cb1f1273db003fbf5c43e2
Raktim Mitra <raktim.mitra@codeclouds.co.in>
2025-11-04 15:39:00 +0530

    Fix CORS origin URL by removing trailing slash for consistency

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(       
   cors({
-    origin: "https://resumine-resume.netlify.app/",
+    origin: "https://resumine-resume.netlify.app",
     credentials:true
   })
 );
