7722d1ed5b52cc38136d8cb6af0d39fd6caafa83
Raktim Mitra <raktim.mitra@codeclouds.co.in>
2025-11-04 16:05:53 +0530

    Fix executable path for Sparticuz Chromium in downloadResume function

--- server/src/controllers/resumeController.js
+++ server/src/controllers/resumeController.js
@@ -101,7 +101,7 @@ export const downloadResume = async (req, res) => {
     const browser = await puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(), // 👈 key line
+      executablePath: await chromium.executablePath(),
       headless: chromium.headless,
       ignoreHTTPSErrors: true,
     });
